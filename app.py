54ff6169a69bb7adaa9d454be84d87a791f2b7c4
Gaurav <gauravsvikhe@gmail.com>
2023-10-04 07:42:58 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+
+import streamlit as st
+from calculator import Calculator
+
+st.title("Work Hours Calculator")
+
+weekly_limit = st.slider("Weekly Limit", 48, 54, step=1)
+daily_limit = st.slider("Daily Limit", 9, 13, step=1)
+
+if st.button("Calculate"):
+    calculator = Calculator()
+    calculator.weekly_regular_limit = weekly_limit
+    calculator.daily_spread_over_limit = daily_limit
+    data = calculator.calculate()
+    st.write(data)  # Display data
